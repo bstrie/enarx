2abd0337337eb32f3217c4fedf9c32eb6de69317
bstrie <865233+bstrie@users.noreply.github.com>
2022-03-11 06:00:37 -0500

    fix shim-kvm

--- src/backend/sev/mod.rs
+++ src/backend/sev/mod.rs
@@ -50,7 +50,7 @@ impl super::Backend for Backend {
 
     #[inline]
     fn shim(&self) -> &'static [u8] {
-        include_bytes!(concat!(env!("OUT_DIR"), "/bin/shim-sev"))
+        include_bytes!(concat!(env!("OUT_DIR"), "/bin/shim-kvm"))
     }
 
     #[inline]
